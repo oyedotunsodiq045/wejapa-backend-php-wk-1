00b8052d2b23ed6bed8ead3645be2e2222801d1e
immanuelredd <66283891+immanuelredd@users.noreply.github.com>
2020-07-21 14:53:09 +0100

    'first-task'

--- send.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['submit'])) {
-    echo '<br>Hello'.$_POST['name'];
-    echo '<br><br>Hope we can work together,  '.$_POST['code'];
-    echo '<br><br> My stack comprises of Html, CSS, and a touch of JS';
-}
-?>
-
-    
