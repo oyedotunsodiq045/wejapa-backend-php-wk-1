187b5f05417c201836949a6980e1e792b5b7e09d
Vikas Rathore <vikasrathour162@gmail.com>
2020-07-24 11:27:54 +0530

    add: Vikas form

---
+++ Vikas/Welcome.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+<h1>Welcome <?php echo ''.$_POST["fname"].' '.$_POST["lname"]; ?>to Wejapa Internship</h1><br>
+<h1>Your email address is: <?php echo $_POST["email"]; ?></h1>
+
+</body>
+</html>
