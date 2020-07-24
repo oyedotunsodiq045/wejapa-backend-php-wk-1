94d31feb7fc462d156d97c51505e212fab7de8ee
identityseal <teemealeheen@gmail.com>
2020-07-24 18:24:32 +0100

    add: identitySeal form

---
+++ identitySeal/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form style="padding-top: 100px; padding-left: 100px"action="app.php" method="post">
+	<h4>Please input your name and your email :</h4>
+
+	<input type="text" name="name" placeholder="Name" />
+
+	<input type="text" name="email" placeholder="Email" />
+
+	<input type="submit" name="post"/>
+
+</form>
+
+	
+
+</body>
+</html>
