274ebecd395c1f244e8136337e75a7460b92eb1f
Obinna Amadi <45544067+donaldamadi@users.noreply.github.com>
2020-07-25 15:32:12 +0200

    add obinna form

---
+++ Obinna/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obinna's Form</title>
+</head>
+
+<body>
+    <h2>Let's get to know each other</h2>
+    <form action="welcome.php" method="POST">
+        Name: <input type="text" name="name" placeholder="Your name please" required>
+        Email address: <input type="email" name="email" placeholder="Email address" required>
+        Phone Number: <input type="number" name="phone" placeholder="phone number">
+        <input type="submit" value="Submit">
+    </form>
+
+</body>
+
+</html>
+
---
+++ Obinna/welcome.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    Welcome to my page <?php echo $_POST["name"]; ?><br>
+    <?php echo $_POST["email"]; ?> is your email address and <?php echo $_POST["number"]?> is your phone number <br>
+    Thank you again, have a great day
+</body>
+</html>
