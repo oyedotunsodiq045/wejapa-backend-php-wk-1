9334ff0fd7cbbfc93ad8196da4335e0afa7c327d
Adetoyebamise <55400020+Adetoyebamise@users.noreply.github.com>
2020-07-25 00:59:00 -0700

    Add files via upload
    Contributing!

---
+++ Submittry.php
@@ -0,0 +1,17 @@
+<html>
+   <head> 
+   <title>Welcome</title>
+   </head>
+	<body>
+<?php
+	$Username = $_POST['Username']";
+	$Stack = $_POST['Stack'];
+	
+	echo 'Hello' . $Username. '<br/>';
+	echo 'Your stack is '. $Stack .'<br/>';
+	echo 'Thank you for dropping your comments.';
+		
+	?>
+  
+	</body>
+</html>
